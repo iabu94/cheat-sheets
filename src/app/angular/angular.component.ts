3516efc4280ea483965b199689ad4c9fab5b3fbd
Abdullah Mansoor <mansoorabdullah001@gmail.com>
2022-08-29 15:53:01 +0530

    Skip Tests

--- src/app/angular/angular.component.ts
+++ src/app/angular/angular.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { map, Observable, startWith } from 'rxjs';
+
+interface Type {
+  displayName: string;
+  key: string;
+  shortKey: string;
+}
 
 @Component({
   selector: 'dh-angular',
@@ -7,9 +15,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AngularComponent implements OnInit {
 
-  constructor() { }
+  formGroup: FormGroup;
+  filteredOptions!: Observable<Type[]>;
 
-  ngOnInit(): void {
+  types: Type[] = [
+    {
+      displayName: 'module',
+      key: 'module',
+      shortKey: 'm'
+    },
+    {
+      displayName: 'component',
+      key: 'component',
+      shortKey: 'c'
+    },
+    {
+      displayName: 'service',
+      key: 'service',
+      shortKey: 's'
+    },
+    {
+      displayName: 'pipe',
+      key: 'pipe',
+      shortKey: 'p'
+    }
+  ];
+
+  constructor(private fb: FormBuilder) {
+    this.formGroup = this.fb.group({
+      name: [''],
+      type: ['module'],
+      skipTests: [true]
+    });
   }
 
+  get f() {
+    return this.formGroup.controls;
+  }
+
+  ngOnInit() {
+    this.filteredOptions = this.f['type'].valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value || '')),
+    );
+  }
+
+  private _filter(value: string): Type[] {
+    const filterValue = value.toLowerCase();
+
+    return this.types.filter(option => option.displayName.toLowerCase().includes(filterValue));
+  }
+
+  getCode() {
+    let code = 'ng g';
+    const type = this.types.find(type => type.key === this.f['type'].value)?.shortKey;
+    code += type ? ` ${type}` : '';
+    const name = this.f['name'].value;
+    code += name ? ` ${name}` : '';
+    code += this.f['skipTests'].value ? ' --skip-tests' : '';
+    return code;
+  }
 }
--- src/app/angular/angular.module.ts
+++ src/app/angular/angular.module.ts
@@ -1,9 +1,13 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from "@angular/material/autocomplete";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatSelectModule } from "@angular/material/select";
 import { AngularRoutingModule } from './angular-routing.module';
 import { AngularComponent } from './angular.component';
-
 
 @NgModule({
   declarations: [
@@ -11,7 +15,13 @@ import { AngularComponent } from './angular.component';
   ],
   imports: [
     CommonModule,
-    AngularRoutingModule
+    AngularRoutingModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatAutocompleteModule,
+    MatSelectModule,
+    MatInputModule,
+    MatCheckboxModule
   ]
 })
 export class AngularModule { }
