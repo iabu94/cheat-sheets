a64be13f41d173c31a99b6ee201e9ef0949198ce
Abdullah Mansoor <mansoorabdullah001@gmail.com>
2022-09-19 13:25:58 +0530

    Deployment Component

--- src/app/angular/deployment/deployment.component.ts
+++ src/app/angular/deployment/deployment.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
+interface Platform {
+  value: string;
+  name: string;
+}
 @Component({
   selector: 'dh-deployment',
   templateUrl: './deployment.component.html',
   styleUrls: ['./deployment.component.scss']
 })
-export class DeploymentComponent implements OnInit {
+export class DeploymentComponent {
+  formGroup: FormGroup;
+  platforms: Platform[];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) {
+    this.formGroup = this.fb.group({
+      platform: ['gh-pages']
+    });
+    this.platforms = [
+      {
+        name: 'GitHub Pages',
+        value: 'gh-pages'
+      }
+    ];
   }
-
 }
